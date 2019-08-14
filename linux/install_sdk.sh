2fd0444317683574dca30346dec3aeb02319a722
Tom <thomas.verin@sonarsource.com>
2019-08-14 11:23:46 +0200

    Update java jdk versions

--- linux/install_sdk.sh
+++ linux/install_sdk.sh
@@ -13,14 +13,14 @@ EOT
 
 sdk list java
 
-sdk install java 12.0.1-zulu < /dev/null
-sdk default java 12.0.1-zulu || exit 1
+sdk install java 12.0.2-zulu < /dev/null
+sdk default java 12.0.2-zulu || exit 1
 
 sdk install java 11.0.2-open < /dev/null
 sdk default java 11.0.2-open || exit 1
 
-sdk install java 11.0.3-zulu < /dev/null
-sdk default java 11.0.3-zulu || exit 1
+sdk install java 11.0.4-zulu < /dev/null
+sdk default java 11.0.4-zulu || exit 1
 
 sdk install java 10.0.2-open < /dev/null
 sdk default java 10.0.2-open || exit 1
@@ -28,10 +28,10 @@ sdk default java 10.0.2-open || exit 1
 sdk install java 9.0.4-open < /dev/null
 sdk default java 9.0.4-open || exit 1
 
-sdk install java 8.0.212-zulu < /dev/null
-sdk default java 8.0.212-zulu || exit 1
+sdk install java 8.0.222-zulu < /dev/null
+sdk default java 8.0.222-zulu || exit 1
 
 sdk install java 6.0.119-zulu < /dev/null
 sdk default java 6.0.119-zulu || exit 1
 
-sdk default java 8.0.212-zulu
+sdk default java 12.0.2-zulu
