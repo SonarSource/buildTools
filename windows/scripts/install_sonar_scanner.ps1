be44438c8dfffce04150fb57225f0b5e5210e947
Tom <thomas.verin@sonarsource.com>
2019-08-15 16:33:21 +0200

    Update windows jdk and sonar scanner versions

--- windows/scripts/install_sonar_scanner.ps1
+++ windows/scripts/install_sonar_scanner.ps1
@@ -3,7 +3,7 @@ $ErrorActionPreference = 'Stop'
 $path = "${env:Temp}\sonar-scanner.zip"
 
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-(New-Object System.Net.WebClient).DownloadFile('https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-3.2.0.1227.zip', $path)
+(New-Object System.Net.WebClient).DownloadFile('https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-4.0.0.1744.zip', $path)
 
 Add-Type -AssemblyName System.IO.Compression.FileSystem
 [System.IO.Compression.ZipFile]::ExtractToDirectory($path, 'C:\')
