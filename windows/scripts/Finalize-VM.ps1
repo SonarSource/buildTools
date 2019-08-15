d9b05d2c7f15afd22e8b608901ce0cf642baf78e
Tom <thomas.verin@sonarsource.com>
2019-08-15 16:33:05 +0200

    Fix windows finalize script bad char

--- windows/scripts/Finalize-VM.ps1
+++ windows/scripts/Finalize-VM.ps1
@@ -3,4 +3,4 @@ Write-Host "Finalize VM configuration"
 $currentPath = (Get-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH).Path
 $updatedPath = $currentPath+';C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin'
 
-Set-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH –Value $updatedPath
+Set-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH -Value $updatedPath
