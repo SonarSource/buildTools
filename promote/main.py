842ad9867620c39924f444a304e855afb0a38ab5
Tom <thomas.verin@sonarsource.com>
2019-09-26 12:26:23 +0200

    Handle bad parameters and return code

--- promote/main.py
+++ promote/main.py
@@ -4,26 +4,33 @@ import requests
 import json
 
 from flask import escape
+from flask import make_response
 
 artifactoryUrl=os.environ.get('ARTIFACTORY_URL','repox url not set')
 artifactoryApiKey=os.environ.get('ARTIFACTORY_API_KEY','not api key provided')  
-
-def getVar(request,var):
-  request_json = request.get_json(silent=True)
-  request_args = request.args
-  value=None
-  if request_json and var in request_json:
-    value = request_json[var]
-  elif request_args and var in request_args:
-    value = request_args[var]
-  return value
+AUTHENTICATED="authenticated"
 
 def repoxGetPropertyFromBuildInfo(project, buildNumber, property):  
   url = f"{artifactoryUrl}/api/build/{project}/{buildNumber}"
   headers = {'content-type': 'application/json', 'X-JFrog-Art-Api': artifactoryApiKey}
   r = requests.get(url, headers=headers)  
   buildInfo = r.json()
-  return buildInfo['buildInfo']['properties'][property]
+  if r.status_code == 200:      
+    return buildInfo['buildInfo']['properties'][property]
+  else:
+    raise Exception('unknown build')
+
+def validateAutorizationHeader(request):
+  if request.headers['Authorization'] and request.headers['Authorization'].split()[0] == 'Bearer':
+    if (request.headers['Authorization'].split()[1] == os.environ.get('ACCESS_TOKEN')):
+      return AUTHENTICATED
+    else:
+      return "Wrong access token"
+  else:
+    return "Missing access token"
+  
+
+
 
 # [START functions_promote_http]
 def promote(request):
@@ -39,62 +46,62 @@ def promote(request):
     {functionBaseUrl}/promote/GITHUB_ORG/GITHUB_PROJECT/GITHUB_BRANCH/BUILD_NUMBER/PULL_REQUEST_NUMBER
   """
   
-  auth = request.authorization
-  print("auth:"+str(auth))
+  if validateAutorizationHeader(request) == AUTHENTICATED:  
+    print("PATH:"+request.path)
+    paths=request.path.split("/")
+    #org=paths[1]
+    project=paths[2]
+    githubBranch=paths[3]
+    buildNumber=paths[4]
+    pullRequest=paths[5]
+    targetRepo=None
+    status=None
+    response=None
+    doPromote = False
 
-  print("PATH:"+request.path)
-  paths=request.path.split("/")
-  org=paths[1]
-  project=paths[2]
-  githubBranch=paths[3]
-  buildNumber=paths[4]
-  pullRequest=paths[5]
-  targetRepo=None
-  status=None
-  response=None
-  doPromote = False
+    print("PULL_REQUEST: " + str(pullRequest))
+    print("GITHUB_BRANCH: " + githubBranch)
+    print("BUILD_NUMBER: " + buildNumber)
+    print("PROJECT: " + project)
 
-  print("PULL_REQUEST: " + str(pullRequest))
-  print("GITHUB_BRANCH: " + githubBranch)
-  print("BUILD_NUMBER: " + buildNumber)
-  print("PROJECT: " + project)
+    try:
+      repo = repoxGetPropertyFromBuildInfo(project, buildNumber, 'buildInfo.env.ARTIFACTORY_DEPLOY_REPO')
+    except Exception as e:
+      return make_response(str(e),400)
 
-  repo = repoxGetPropertyFromBuildInfo(project, buildNumber, 'buildInfo.env.ARTIFACTORY_DEPLOY_REPO')
+    if pullRequest:
+      targetRepo = repo.replace('qa', 'dev')
+      status = 'it-passed-pr'
+      doPromote = True
+    else:
+      if githubBranch == "master" or githubBranch.startswith("branch-"):
+          targetRepo = repo.replace('qa', 'builds')
+          status = 'it-passed'
+          doPromote = True
 
-  if pullRequest:
-    print("in PR")
-    targetRepo = repo.replace('qa', 'dev')
-    status = 'it-passed-pr'
-    doPromote = True
-  else:
-    print("NOT in PR")
-    if githubBranch == "master" or githubBranch.startswith("branch-"):
-        targetRepo = repo.replace('qa', 'builds')
+      if githubBranch.startswith("dogfood-on-"):
+        targetRepo = "sonarsource-dogfood-builds"
         status = 'it-passed'
         doPromote = True
 
-    if githubBranch.startswith("dogfood-on-"):
-      targetRepo = "sonarsource-dogfood-builds"
-      status = 'it-passed'
-      doPromote = True
-
-  print(f"status:{status}")
-  
-  if doPromote:
-    print(f"Promoting build {project}#{buildNumber}")
-    json_payload={
-        "status": f"{status}",
-        "targetRepo": f"{targetRepo}"
-    }
-    print(json_payload)
-    url = f"{artifactoryUrl}/api/build/promote/{project}/{buildNumber}"
-    headers = {'content-type': 'application/json', 'X-JFrog-Art-Api': artifactoryApiKey}
-    r = requests.post(url, data=json.dumps(json_payload), headers=headers)
-    response = f"promotion http_code: {r.status_code} json: {r.json} text: {r.text}"
+    print(f"status:{status}")
     
+    if doPromote:
+      print(f"Promoting build {project}#{buildNumber}")
+      json_payload={
+          "status": f"{status}",
+          "targetRepo": f"{targetRepo}"
+      }
+      url = f"{artifactoryUrl}/api/build/promote/{project}/{buildNumber}"
+      headers = {'content-type': 'application/json', 'X-JFrog-Art-Api': artifactoryApiKey}
+      r = requests.post(url, data=json.dumps(json_payload), headers=headers)
+      if r.status_code == 200:      
+        return make_response(f"status:{status}",200)
+      else:
+        return make_response(r.text,r.status_code)
+    else:
+      response = "No promotion for builds coming from a development branch"
+    return response
   else:
-    response = "No promotion for builds coming from a development branch"
-
-  print(response)
-  return response
+    return make_response(validateAutorizationHeader(request),403)
 # [END functions_promote_http]
