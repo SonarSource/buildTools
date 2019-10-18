1d0555de3de2e7ac9ffa0f744abdae8b89afc893
Tom <thomas.verin@sonarsource.com>
2019-10-18 10:32:36 +0200

    install buildTools and add them to path

--- windows/scripts/Finalize-VM.ps1
+++ windows/scripts/Finalize-VM.ps1
@@ -1,6 +1,6 @@
 Write-Host "Finalize VM configuration"
 
 $currentPath = (Get-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH).Path
-$updatedPath = $currentPath+';C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin'
+$updatedPath = $currentPath+';C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin;c:\buildTools-docker\bin'
 
 Set-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH -Value $updatedPath
