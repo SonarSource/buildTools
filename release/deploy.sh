ca21cf4647bc3b8021ef31d3824524d2b1a107ff
Tom <thomas.verin@sonarsource.com>
2020-02-17 16:12:31 +0100

    Fix deploy script

--- release/deploy.sh
+++ release/deploy.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 export GOOGLE_PROJECT_ID=language-team
-. ./secret.sh
+. ./secrets.sh
 
 gcloud functions deploy release \
   --runtime python37 --trigger-http \
   --set-env-vars ARTIFACTORY_API_KEY=$ARTIFACTORY_API_KEY,GPG_PASSPHRASE=$GPG_PASSPHRASE \
   --region us-central1 \
-  --source=src \
-  --memory 128MB --project $GOOGLE_PROJECT_ID
+  --memory 128MB \
+  --project $GOOGLE_PROJECT_ID \
+  --source src
