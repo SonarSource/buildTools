3b433eab9a75fa52ca749c2c8dac758bb74d128c
Tom <thomas.verin@sonarsource.com>
2020-02-25 09:59:29 +0100

    Update path-prefix to production settings

--- release/deploy.sh
+++ release/deploy.sh
@@ -2,11 +2,12 @@
 
 export GOOGLE_PROJECT_ID=language-team
 . ./secrets.sh
-#/sonarsource/var/opt/sonarsource/binaries.sonarsource.com/sonarsource.bintray.com/
+
+PATH_PREFIX=/sonarsource/var/opt/sonarsource/binaries.sonarsource.com/sonarsource.bintray.com/
 
 gcloud functions deploy release \
   --runtime python37 --trigger-http \
-  --set-env-vars ARTIFACTORY_API_KEY=$ARTIFACTORY_API_KEY,GPG_PASSPHRASE=$GPG_PASSPHRASE,PATH_PREFIX=/tmp,BURGRX_USER=$BURGRX_USER,BURGRX_PASSWORD=$BURGRX_PASSWORD \
+  --set-env-vars ARTIFACTORY_API_KEY=$ARTIFACTORY_API_KEY,GPG_PASSPHRASE=$GPG_PASSPHRASE,PATH_PREFIX=$PATH_PREFIX,BURGRX_USER=$BURGRX_USER,BURGRX_PASSWORD=$BURGRX_PASSWORD \
   --region us-central1 \
   --memory 128MB \
   --project $GOOGLE_PROJECT_ID \
