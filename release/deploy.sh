f82995aa5af2a665928db9288f340604c83c4468
Tom <thomas.verin@sonarsource.com>
2020-02-27 09:00:28 +0100

    Update deployment

--- release/deploy.sh
+++ release/deploy.sh
@@ -7,7 +7,7 @@ PATH_PREFIX=/sonarsource/var/opt/sonarsource/binaries.sonarsource.com/sonarsourc
 
 gcloud functions deploy release \
   --runtime python37 --trigger-http \
-  --set-env-vars ARTIFACTORY_API_KEY=$ARTIFACTORY_API_KEY,GPG_PASSPHRASE=$GPG_PASSPHRASE,PATH_PREFIX=$PATH_PREFIX,BURGRX_USER=$BURGRX_USER,BURGRX_PASSWORD=$BURGRX_PASSWORD \
+  --set-env-vars ARTIFACTORY_API_KEY=$ARTIFACTORY_API_KEY,GPG_PASSPHRASE=$GPG_PASSPHRASE,PATH_PREFIX=$PATH_PREFIX,BURGRX_USER=$BURGRX_USER,BURGRX_PASSWORD=$BURGRX_PASSWORD,CIRRUS_TOKEN=$CIRRUS_TOKEN \
   --region us-central1 \
   --memory 128MB \
   --project $GOOGLE_PROJECT_ID \
