b144cb3ba92aca42f774029b63b4500ee7722f08
Tom <thomas.verin@sonarsource.com>
2020-03-31 15:44:15 +0200

    Fix path prefix

--- release/deploy.sh
+++ release/deploy.sh
@@ -3,11 +3,9 @@
 export GOOGLE_PROJECT_ID=language-team
 . ./secrets.sh
 
-PATH_PREFIX=/sonarsource/var/opt/sonarsource/binaries.sonarsource.com/sonarsource.bintray.com/
-
 gcloud functions deploy release \
   --runtime python37 --trigger-http \
-  --set-env-vars ARTIFACTORY_API_KEY=$ARTIFACTORY_API_KEY,GPG_PASSPHRASE=$GPG_PASSPHRASE,PATH_PREFIX=$PATH_PREFIX,BURGRX_USER=$BURGRX_USER,BURGRX_PASSWORD=$BURGRX_PASSWORD,CIRRUS_TOKEN=$CIRRUS_TOKEN \
+  --set-env-vars ARTIFACTORY_API_KEY=$ARTIFACTORY_API_KEY,GPG_PASSPHRASE=$GPG_PASSPHRASE,PATH_PREFIX=/sonarsource/var/opt/sonarsource/binaries.sonarsource.com/sonarsource.bintray.com/,BURGRX_USER=$BURGRX_USER,BURGRX_PASSWORD=$BURGRX_PASSWORD,CIRRUS_TOKEN=$CIRRUS_TOKEN \
   --region us-central1 \
   --memory 128MB \
   --timeout 360 \
