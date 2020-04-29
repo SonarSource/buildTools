10538bd9b0df4cd3d34483032731df1e91a9ea8e
Tom <thomas.verin@sonarsource.com>
2020-04-29 11:36:08 +0200

    deploy with 2048mb ofp memory

--- release/deploy.sh
+++ release/deploy.sh
@@ -7,7 +7,7 @@ gcloud functions deploy release \
   --runtime python37 --trigger-http \
   --set-env-vars ARTIFACTORY_API_KEY=$ARTIFACTORY_API_KEY,GPG_PASSPHRASE=$GPG_PASSPHRASE,PATH_PREFIX=/sonarsource/var/opt/sonarsource/binaries.sonarsource.com/sonarsource.bintray.com/,BURGRX_USER=$BURGRX_USER,BURGRX_PASSWORD=$BURGRX_PASSWORD,CIRRUS_TOKEN=$CIRRUS_TOKEN \
   --region us-central1 \
-  --memory 128MB \
+  --memory 2048MB \
   --timeout 360 \
   --project $GOOGLE_PROJECT_ID \
   --source src
