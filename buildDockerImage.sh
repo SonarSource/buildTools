b5024deb008a0f661c9f9344220153cd0d103be2
Tom <thomas.verin@sonarsource.com>
2021-04-01 15:29:10 +0200

    Fix name

--- buildDockerImage.sh
+++ buildDockerImage.sh
@@ -13,8 +13,8 @@ elif [ $CIRRUS_BRANCH == "docker" ]; then
   ./build.sh -t $TAG
   ./build.sh -t latest
   ./build.sh -t $TAG -f Dockerfile-jdk15 -n base-jdk15
-  ./build.sh -t latest -f Dockerfile-jdk15 -n base-jdk16
-  ./build.sh -t $TAG -f Dockerfile-jdk16 -n base-jdk15
+  ./build.sh -t latest -f Dockerfile-jdk15 -n base-jdk15
+  ./build.sh -t $TAG -f Dockerfile-jdk16 -n base-jdk16
   ./build.sh -t latest -f Dockerfile-jdk16 -n base-jdk16
 else
   echo "Not building image for feature branch"
