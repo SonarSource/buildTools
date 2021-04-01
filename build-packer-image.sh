27f70c9bffa4dd383c793b9574c488a4a706aef5
Tom <thomas.verin@sonarsource.com>
2021-04-01 16:00:37 +0200

    Add jdk15 windows image

--- build-packer-image.sh
+++ build-packer-image.sh
@@ -7,8 +7,8 @@ TAG=v1
 if [ "${CIRRUS_PR:-}" != "" ]; then
   TAG=$CIRRUS_PR
   export IMAGE_FAMILY=
-elif [ $CIRRUS_BRANCH == "lt-base-windows" ]; then
-  export IMAGE_FAMILY=lt-base-windows
+elif [ $CIRRUS_BRANCH == "lt-base-windows-jdk15" ]; then
+  export IMAGE_FAMILY=lt-base-windows-jdk15
 else
   echo "Not building image for feature branch"
 fi
@@ -19,4 +19,4 @@ apt-get update && apt-get -y install curl unzip
 curl -LsS https://releases.hashicorp.com/packer/1.3.5/packer_1.3.5_linux_amd64.zip > packer.zip
 unzip packer.zip
 echo ${ACCOUNT} > account.json
-GOOGLE_APPLICATION_CREDENTIALS=account.json ./packer build -force windows/lt-base-windows.json
+GOOGLE_APPLICATION_CREDENTIALS=account.json ./packer build -force windows/lt-base-windows-jdk15.json
