1720489ea3d13c66c2ea9f85516933c280b358c9
Costin Zaharia <costin.zaharia@sonarsource.com>
2021-04-23 15:19:03 +0200

    Remove dotnet 3.0.103 and update visual studio

--- windows/scripts/Install-DotNetSdk.ps1
+++ windows/scripts/Install-DotNetSdk.ps1
@@ -1,4 +1,3 @@
 choco install dotnetcore-sdk --version 2.1.815
-choco install dotnetcore-sdk --version 3.0.103
 choco install dotnetcore-sdk --version 3.1.408
 choco install dotnet-sdk --version 5.0.202
