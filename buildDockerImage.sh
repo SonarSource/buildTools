decc7de8d294f8d575c48289f19ca71bd4226245
Alban Auzeill <alban.auzeill@sonarsource.com>
2022-01-14 16:58:11 +0100

    Add new jdk17 to be used by sonar-java build (#44)

--- buildDockerImage.sh
+++ buildDockerImage.sh
@@ -8,7 +8,6 @@ if [ "${CIRRUS_PR:-}" != "" ]; then
   ./build.sh -t "PR_$CIRRUS_PR"
   ./build.sh -t "PR_$CIRRUS_PR" -f Dockerfile-jdk15 -n base-jdk15
   ./build.sh -t "PR_$CIRRUS_PR" -f Dockerfile-jdk16 -n base-jdk16
-  
 elif [ $CIRRUS_BRANCH == "docker" ]; then
   ./build.sh -t $TAG
   ./build.sh -t latest
@@ -16,6 +15,8 @@ elif [ $CIRRUS_BRANCH == "docker" ]; then
   ./build.sh -t latest -f Dockerfile-jdk15 -n base-jdk15
   ./build.sh -t $TAG -f Dockerfile-jdk16 -n base-jdk16
   ./build.sh -t latest -f Dockerfile-jdk16 -n base-jdk16
+  ./build.sh -t $TAG -f Dockerfile-jdk17 -n base-jdk17
+  ./build.sh -t latest -f Dockerfile-jdk17 -n base-jdk17
 else
   echo "Not building image for feature branch"
 fi
