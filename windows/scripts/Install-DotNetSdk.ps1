f3c9e29e9b698751f635c60479a43e9b7ad38e8d
Čaba Šagi <csaba.sagi@sonarsource.com>
2022-03-02 13:24:58 +0100

    Install net6

--- windows/scripts/Install-DotNetSdk.ps1
+++ windows/scripts/Install-DotNetSdk.ps1
@@ -1,3 +1,4 @@
 choco install dotnetcore-sdk --version 2.1.815
 choco install dotnetcore-sdk --version 3.1.408
 choco install dotnet-sdk --version 5.0.202
+choco install dotnet-sdk --version 6.0.200
